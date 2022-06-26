b3a5c9caa0cc3754cb32a9f4afe4a5d34798c476
Alexsandro Souza <asouza@deem.com>
2022-06-26 10:56:47 +0100

    refactoring actions

--- eta.js
+++ eta.js
@@ -1,7 +1,7 @@
 // Start observing mutation of element. On change, the
 //   the callback is called with Boolean visibility as
 //   argument:
-function createBtn(){
+function createBtn() {
     // create a new div element
     const newDiv = document.createElement("button");
     newDiv.textContent = "run automate test"
@@ -10,17 +10,18 @@ function createBtn(){
     newDiv.style.right = '0';
     newDiv.style.zIndex = '100';
     newDiv.style.top = '0';
-    newDiv.addEventListener("click", function(e) {
+    newDiv.addEventListener("click", function (e) {
+        waitDepartCalendar();
+        waitModalStationTo();
         // select train segment
         document.querySelector("#root > div > div:nth-child(1) > div > div > div > div > button:nth-child(2)").click()
         // select From
         document.querySelector("#root > div > div > div > div > div > div:nth-child(3) > div > div > button:nth-child(1)").click()
-        // select To
-        document.querySelector("#root > div > div > div > div > div > div:nth-child(3) > div > div > button:nth-child(2)").click()
     })
     document.querySelector("body").appendChild(newDiv)
 
 }
+
 function respondToMutation(element, callback) {
     // create an observer instance
     var observer = new MutationObserver(function (mutations) {
@@ -39,6 +40,91 @@ function respondToMutation(element, callback) {
 
 modalHandle = "from"
 stationFrom = ""
+
+function waitJourneysPage() {
+    let loadJourneysInterval = setInterval(function () {
+        // Waiting the journeys to be loaded
+        if (document.querySelector("#root > div > div:nth-child(1) > div > div > div > div:nth-child(2) div").children.length > 3) {
+            clearInterval(loadJourneysInterval)
+            // Select journey
+            document.querySelector("#root > div > div:nth-child(1) > div > div > div > div > div > div:nth-child(3) > div").click()
+            modalHandle = "fare"
+        }
+    }, 1000)
+}
+
+function waitPreferencePage() {
+    let loadRtpInterval = setInterval(function () {
+        let getPreferenceBtn = document.querySelector("#root > div > div:nth-child(1) > div > div > div > div > div > div > div > div > a");
+        if (getPreferenceBtn) {
+            clearInterval(loadRtpInterval)
+            console.log("Open Preference modal")
+            getPreferenceBtn.click()
+            modalHandle = "preference"
+        }
+    }, 1000)
+}
+
+function waitTravelInfoPage() {
+    let loadTravelInfoInterval = setInterval(function () {
+        let continueBtn = document.querySelector("#root button[data-tracking-id='traveler-info-continue-button']");
+        if (continueBtn) {
+            clearInterval(loadTravelInfoInterval)
+            console.log("Press continue button")
+            continueBtn.click()
+            modalHandle = "continue"
+        }
+    }, 1000)
+}
+
+function waitPaymentPage() {
+    let loadPaymentInterval = setInterval(function () {
+        let bookNowBtn = document.querySelector("#root button[data-tracking-id='book-trip-button']");
+        if (bookNowBtn) {
+            console.log("Press Book now button")
+            clearInterval(loadPaymentInterval)
+            setTimeout(function () {
+                bookNowBtn.click()
+            }, 500)
+            modalHandle = "book"
+        }
+    }, 1000)
+}
+
+function waitDepartCalendar() {
+    let interval = setInterval(function () {
+        let calendarDayEl = document.querySelector(".CalendarDay[aria-disabled='false']:last-child");
+        if (modalHandle === "date" && calendarDayEl) {
+            console.log("Selecting date")
+            clearInterval(interval)
+            calendarDayEl.click()
+            document.querySelector("#modals-container > div > div > div > button").click()
+            modalHandle = "closeDate"
+        }
+    }, 1000)
+}
+
+function waitModalStationTo() {
+    let interval = setInterval(function () {
+        let element = document.querySelector("#modals-container > div > div li");
+        if (modalHandle === "to" && element) {
+            console.log("Selecting station TO")
+            clearInterval(interval)
+            console.log(stationFrom, " ", element.textContent)
+            if (stationFrom === element.textContent) {
+                element = document.querySelector("#modals-container > div > div li:last-child")
+            }
+            console.log(stationFrom, " - ", element.textContent)
+            element.click()
+            modalHandle = "closeTo"
+            setTimeout(function () {
+                // click date btn
+                document.querySelector("#root > div > div > div > div > div > div:nth-child(4) > div > div > button:nth-child(1)").click()
+            }, 1000)
+        }
+    }, 1000)
+}
+
 respondToMutation(document.querySelector("#modals-container"), function (i) {
     console.log(modalHandle)
     if (modalHandle === "from") {
@@ -46,46 +132,18 @@ respondToMutation(document.querySelector("#modals-container"), function (i) {
         let selector = document.querySelector("#modals-container > div > div li");
         stationFrom = selector.textContent
         selector.click()
-        modalHandle = "closeFrom"
-        return
-    }
-
-    if (modalHandle === "to") {
-        console.log("Selecting station TO")
-        let element = document.querySelector("#modals-container > div > div li");
-        console.log(stationFrom, " ", element.textContent)
-        if (stationFrom === element.textContent) {
-            element = document.querySelector("#modals-container > div > div li:last-child")
-        }
-        console.log(stationFrom, " ", element.textContent)
-        element.click()
-        modalHandle = "closeTo"
+        modalHandle = "to"
         setTimeout(function () {
-            // select date
-            document.querySelector("#root > div > div > div > div > div > div:nth-child(4) > div > div > button:nth-child(1)").click()
-        }, 1000)
-    }
-
-    if (modalHandle === "date") {
-        console.log("Selecting date")
-        document.querySelector(".CalendarDay[aria-disabled='false']:last-child").click()
-        document.querySelector("#modals-container > div > div > div > button").click()
-        modalHandle = "closeDate"
+            document.querySelector("#root > div > div > div > div > div > div:nth-child(3) > div > div > button:nth-child(2)").click()
+        }, 500)
+        return
     }
 
     if (modalHandle === "fare") {
         console.log("Selecting fare")
         document.querySelector("#modals-container > div > div > div > div > div > div > button").click()
         modalHandle = "closeFare"
-        loadRtpInterval = setInterval(function () {
-            let getPreferenceBtn = document.querySelector("#root > div > div:nth-child(1) > div > div > div > div > div > div > div > div > a");
-            if (getPreferenceBtn){
-                clearInterval(loadRtpInterval)
-                console.log("Open Preference modal")
-                getPreferenceBtn.click()
-                modalHandle = "preference"
-            }
-        }, 1000)
+        waitPreferencePage();
     }
     if (modalHandle === "preference") {
         console.log("Select Preference")
@@ -93,38 +151,13 @@ respondToMutation(document.querySelector("#modals-container"), function (i) {
         modalHandle = "closePreference"
     }
     if (modalHandle === "closePreference") {
-        console.log("checkout selected")
+        console.log("checkout btn clicked")
         document.querySelector("#root button[data-tracking-id='rtp-checkout-button']").click()
-        loadTravelInfoInterval = setInterval(function () {
-            let continueBtn = document.querySelector("#root button[data-tracking-id='traveler-info-continue-button']");
-            if (continueBtn){
-                clearInterval(loadTravelInfoInterval)
-                console.log("Press continue button")
-                continueBtn.click()
-                modalHandle = "continue"
-            }
-        }, 1000)
-
-        loadPaymentInterval = setInterval(function () {
-            let bookNowBtn = document.querySelector("#root button[data-tracking-id='book-trip-button']");
-            if (bookNowBtn){
-                console.log("Press Book now button")
-                bookNowBtn.click()
-                clearInterval(loadPaymentInterval)
-                modalHandle = "book"
-            }
-        }, 1000)
+        waitTravelInfoPage();
+        waitPaymentPage();
         modalHandle = "checkout"
     }
 
-    if (modalHandle === "closePreference") {
-        console.log("Closing Preference modal")
-        modalHandle = "checkout"
-    }
-    if (modalHandle === "closeFrom") {
-        console.log("Closing From modal")
-        modalHandle = "to"
-    }
 
     if (modalHandle === "closeTo") {
         console.log("Closing To modal")
@@ -134,14 +167,7 @@ respondToMutation(document.querySelector("#modals-container"), function (i) {
         console.log("Closing Date modal")
         setTimeout(function () {
             document.querySelector("#root > div > div > div > div > div > div:nth-child(6) button").click()
-            loadJourneysInterval = setInterval(function () {
-                // Waiting the journeys to be loaded
-                if(document.querySelector("#root > div > div:nth-child(1) > div > div > div > div:nth-child(2) div").children.length > 3){
-                    clearInterval(loadJourneysInterval)
-                    document.querySelector("#root > div > div:nth-child(1) > div > div > div > div > div > div:nth-child(3) > div").click()
-                    modalHandle = "fare"
-                }
-            }, 1000)
+            waitJourneysPage();
         }, 1000)
         modalHandle = ""
     }
